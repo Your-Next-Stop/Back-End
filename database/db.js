c4fca94a707598183c32b7553d3aff8888b89502
Raphael Khan <raphaelkhan8@gmail.com>
2020-08-20 14:38:19 -0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,21 +1,7 @@
 const Sequelize = require('sequelize');
 
 const {
-  // DATABASE,
-  // USER_NAME,
-  // USER_PASSWORD,
-  // HOST,
-  // PORT,
   DATABASE_URL,
 } = process.env;
 
 module.exports = new Sequelize(DATABASE_URL);
-
-// module.exports = new Sequelize(DATABASE, USER_NAME, USER_PASSWORD,
-//   {
-//     host: HOST,
-//     port: PORT,
-//     dialect: 'postgres',
-//   });
-
-// module.exports = new Sequelize('postgres://postgres:postgres@localhost:5432/your-next-stop');
